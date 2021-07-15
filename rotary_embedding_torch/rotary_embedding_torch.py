c5bdcf80b6d5b482222329fada4a984668bd44b5
Phil Wang <lucidrains@gmail.com>
2021-07-14 22:16:47 -0700

    add ability to do learned rotations, in preparation for gating experiment

--- rotary_embedding_torch/rotary_embedding_torch.py
+++ rotary_embedding_torch/rotary_embedding_torch.py
@@ -2,7 +2,7 @@ from math import pi, log
 import torch
 from torch import nn, einsum
 
-from einops import rearrange
+from einops import rearrange, repeat
 
 # helper functions
 
@@ -36,10 +36,18 @@ def rotate_half(x):
 
 def apply_rotary_emb(freqs, t, start_index = 0):
     rot_dim = freqs.shape[-1]
+    end_index = start_index + rot_dim
     assert rot_dim <= t.shape[-1], f'feature dimension {t.shape[-1]} is not of sufficient size to rotate in all the positions {rot_dim}'
-    t_left, t, t_right = t[..., :start_index], t[..., start_index:rot_dim], t[..., rot_dim:]
+    t_left, t, t_right = t[..., :start_index], t[..., start_index:end_index], t[..., end_index:]
     t = (t * freqs.cos()) + (rotate_half(t) * freqs.sin())
     return torch.cat((t_left, t, t_right), dim = -1)
+
+# learned rotation helpers
+
+def apply_learned_rotations(rotations, t, start_index = 0):
+    rotations = repeat(rotations, '... n -> ... (n r)', r = 2)
+    print(rotations.shape, t.shape)
+    return apply_rotary_emb(rotations, t, start_index = start_index)
 
 # classes
 
@@ -79,9 +87,8 @@ class RotaryEmbedding(nn.Module):
 
         freqs = self.freqs
 
-        freqs = torch.einsum('i, j -> i j', t.type(freqs.dtype), freqs)
-        freqs = torch.stack((freqs, freqs), dim = -1)
-        freqs = rearrange(freqs, '... n r -> ... (n r)')
+        freqs = torch.einsum('..., f -> ... f', t.type(freqs.dtype), freqs)
+        freqs = repeat(freqs, '... n -> ... (n r)', r = 2)
 
         if exists(cache_key):
             self.cache[cache_key] = freqs
