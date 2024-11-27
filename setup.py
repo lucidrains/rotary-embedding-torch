2c37d7a13756faaa0fee30029c837fdd888b6ffb
lucidrains <lucidrains@gmail.com>
2024-11-27 05:18:37 -0800

    0.8.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'rotary-embedding-torch',
   packages = find_packages(),
-  version = '0.8.5',
+  version = '0.8.6',
   license='MIT',
   description = 'Rotary Embedding - Pytorch',
   long_description_content_type = 'text/markdown',
